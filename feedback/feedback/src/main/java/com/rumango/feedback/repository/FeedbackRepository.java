89624d01b4f0d8dd620b69d731a86cbf51267271
TARNALA SRIBATSA PATRO <153512402+TARNALA-SRIBATSA-PATRO@users.noreply.github.com>
2025-06-13 13:24:57 +0530

    Delete feedback/feedback directory

--- feedback/feedback/src/main/java/com/rumango/feedback/repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rumango.feedback.repository;
-
-import com.rumango.feedback.entity.EmployeeFeedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<EmployeeFeedback, Integer> {
-}
